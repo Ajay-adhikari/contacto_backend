8c997e909098c93fd1501e439f5d7f5e9ed3c1de
Ajay Adhikari <ajayadhi23@gmail.com>
2024-06-23 17:22:31 +0530

    add ons

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,7 +6,8 @@ const DUMMY_CREDENTIALS = { username: 'saltman', password: 'oai1122' };
 
 const login = (req, res) => {
     const { username, password } = req.body;
-    console.log(username , password)
+    // console.log(username , password)
+    //sds
     if (username === DUMMY_CREDENTIALS.username && password === DUMMY_CREDENTIALS.password) {
         const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' });
         res.json({ token });
